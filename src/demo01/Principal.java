86a52c405eab43b50c5bf9935f75aab411260918
eduardogr1984 <eduardogr1984@gmail.com>
2016-12-28 20:48:28 +0100

    Añadida funcionalidad de resta, y mensajes de resta en archivo 'Principal'

--- src/demo01/Principal.java
+++ src/demo01/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		int num1, num2, num3;
-		int suma = 0;
+		int suma = 0, resta = 0;
 		
 		num1 = 5;
 		num2 = 3;
@@ -16,6 +16,10 @@ public class Principal {
 		
 		suma = Sum.suma3Nums(num1,  num2,  num3);
 		System.out.println("\n\nLa suma de 3 números es: " + suma);
+		
+		resta = Res.resta2Nums(num1, num2);
+		System.out.println("\n\nLa resta de 2 números es: " + resta);
+		
 	}
 
 }
---
+++ src/demo01/Res.java
@@ -0,0 +1,14 @@
+package demo01;
+
+public class Res {
+
+	public static int resta2Nums(int n1, int n2) {
+		
+		int res = 0;
+		
+		res = n1 - n2;
+		
+		return res;
+	}
+	
+}
